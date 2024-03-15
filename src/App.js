2ea7193ef81007875e71269e0b0b1cc9f26f39c8
HITARTH3452 <hitarthsingh2201@gmail.com>
2024-03-15 16:24:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import {data} from './Const'
+
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+
+     if(amount<0) amount = 1;
+     if(amount > 10) alert("What will you do of such paras, So give less input");
+     setText(data.slice(0,amount));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Tired of Lorem Ipsum?</h3>
+
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label>Paragraph:</label>
+        <input type="number" name="amount" id="amount" value={count} onChange={(e) => setCount(e.target.value)}></input>
+        <button className='btn'>Generate</button> 
+        </form>  
+
+        <div>
+          {text.map((data , index) => (
+            <p key = {index}> {data} </p>
+          ))}
+        </div>  
     </div>
   );
 }
